46b08c7c977f6dca0d2498811b9895b20c3d64ee
Ender <lombardi.ender@gmail.com>
2017-01-14 17:02:56 +0100

    Premiere implementation de la classe Document
    il reste  les methodes avec une tilde

--- src/biblio/om/Document.java
+++ src/biblio/om/Document.java
@@ -1,5 +1,78 @@
 package biblio.om;
 
 
-class Document {
+public class Document {
+	private static int docTotal = 0;
+	private int _numInv = 0;
+	private String _titre;
+	private int _pages;
+	private Date _creationDate;
+	
+	public Document(String titre, int pages) {
+		this._titre = titre;
+		this._pages = pages;
+	}
+	
+	public int getPages() {
+		return(this._pages)
+	}
+	
+	public void setPages(int pages) {
+		this._pages = pages;
+	}
+	
+	public Date getDate() {
+		return(this._creationDate)
+	}
+	
+	public String getTitre() {
+		return(this._titre)
+	}
+	
+	public void setTitre(String titre) {
+		this._titre = titre;
+	}
+	
+	public String toString() {
+		String phrase_descriptive;
+		phrase_descriptive = "Titre : " + this._titre + " ; Pages : " + this._pages + " ; numInv : " + Integer.toString(this._numInv); // convertir date en string
+	}
+	
+	
+	public static int getNumberOfDocumentCreated() {
+		return(Document.docTotal)
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
