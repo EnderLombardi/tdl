8c8fa28e561a2ac2c9d1b1deff97db7f9cc68093
Ender <lombardi.ender@gmail.com>
2017-01-14 17:25:53 +0100

    Implementation du squelette du main, dans la classe Gestionnaire

--- src/biblio/iu/Gestionnaire.java
+++ src/biblio/iu/Gestionnaire.java
@@ -3,6 +3,7 @@ package biblio.iu;
 
 public class Gestionnaire {
 	
+	private Vector tampon;
 	/**
 	 * Method Gestionnaire
 	 *
@@ -10,5 +11,22 @@ public class Gestionnaire {
 	 */
 	public Gestionnaire() {
 		// TODO: Add your code here
-	}	
+	}
+	
+	public static void main() {
+		Gestionnaire gest = new Gestionnaire();
+		System.out.println("\n==== SCENARIO 1 ====\n");
+		gest.scenario1();
+		//System.out.println("\n==== SCENARIO 2 ====\n");
+		//gest.scenario2();
+	}
+	
+	private void scenario1() {
+		
+	}
+	
+	private void scenario2() {
+		
+	}
+	
 }
