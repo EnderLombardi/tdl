7befa9811a2d1155aa276f9a93484a5202837bf4
Ender <lombardi.ender@gmail.com>
2017-01-15 16:24:45 +0100

    Implementation de la classe Livre

--- src/biblio/om/Livre.java
+++ src/biblio/om/Livre.java
@@ -3,12 +3,38 @@ package biblio.om;
 
 public class Livre {
 	
-	/**
-	 * Method Livre
-	 *
-	 *
-	 */
-	public Livre() {
-		// TODO: Add your code here
-	}	
+	private String _auteur = "";
+	private String _editeur = "";
+	private int _numExemplaire = 0;
+	
+	
+	public Livre(String auteur, String editeur) {
+		this._auteur = auteur;
+		this._editeur = editeur;
+	}
+	
+	
+	public String getAuteur() {
+		return(_auteur);
+	}
+
+	public String getEditeur() {
+		return(_editeur);
+	}
+
+	public void setAuteur(String auteur) {
+		this._auteur = auteur;
+	}
+
+	public void setEditeur(String editeur) {
+		this._editeur = editeur;
+	}
+	
+	public String toString() {
+		String phrase_descriptive;
+		phrase_descriptive = "Auteur : " + this._auteur + " ; Editeur : " + this._editeur + " ; Numero d exemplaire : " + this._numExemplaire;
+		return(phrase_descriptive);
+	}
+		
 }
+	
